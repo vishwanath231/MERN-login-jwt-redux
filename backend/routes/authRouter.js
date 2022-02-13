be9becc7b70223fba412153ad5b7b4bdd91f6ae1
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-13 09:19:48 +0530

    User router

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,39 +1,14 @@
 import { Router } from 'express';
 const router = Router();
-import { registerUser, loginUser } from '../controller/authController.js'
-import auth from '../middleware/authMiddleware.js';
-import User from '../model/authModel.js';
+import { registerUser, loginUser, getuser } from '../controller/authController.js'
+import protect from '../middleware/authMiddleware.js';
 
 
-router
-.route('/register')
-.post(registerUser)
+router.route('/register').post(registerUser);
 
+router.route('/login').post(loginUser);
 
-router
-.route('/login')
-.post(loginUser)
+router.route('/user').get(protect, getuser);
 
-
-/**
- * @route   GET api/auth/user
- * @desc    Get user data
- * @access  Private
- */
-
-router.get('/user', auth, async (req, res) => {
-
-    try {
-
-        const user = await User.findById(req.user.id).select("-password");
-        if (!user) throw Error('User does not exist');
-        res.json(user);
-        
-    } catch (e) {
-        res.status(400).json({
-            msg: e.message
-        })
-    }
-})
 
 export default router;
